d6dfe3a56cd093210e9841eb88707744e21cb490
arindakhill <arinda.hillary@gmail.com>
2024-11-10 22:03:34 -0600

    first commit

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : hills
+// Version     :
+// Copyright   : dont share me
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
